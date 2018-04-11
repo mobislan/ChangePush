fd8e6f71be9f04ac812ec200b1788bf0320ff612
mobislan <mobislan@users.noreply.github.com>
2018-04-11 21:18:03 +0200

    add ionic cordova plugin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,58 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Platform, AlertController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
 import { HomePage } from '../pages/home/home';
+import { Push, PushObject, PushOptions} from '@ionic-native/push';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(public platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public alertCtrl:AlertController, public push: Push) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
+      this.pushsetup();
       splashScreen.hide();
     });
   }
-}
 
+  pushsetup() {
+    const options: PushOptions = {
+     android: {
+         senderID: '31428372280',
+         topics:['nfl_change'],
+         vibrate:true,
+         sound:true,
+         forceShow:true
+     },
+     ios: {
+         alert: 'true',
+         badge: true,
+         sound: 'true'
+     },
+     windows: {}
+  };
+
+  const pushObject: PushObject = this.push.init(options);
+
+  pushObject.on('notification').subscribe((notification: any) => {
+    if (notification.additionalData.foreground) {
+      let youralert = this.alertCtrl.create({
+        title: 'New Push notification',
+        message: notification.message
+      });
+      youralert.present();
+    }
+  });
+
+  pushObject.on('registration').subscribe((registration: any) => {
+     //do whatever you want with the registration ID
+  });
+
+  pushObject.on('error').subscribe(error => alert('Error with Push plugin' + error));
+  }
+}
